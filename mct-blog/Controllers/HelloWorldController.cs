52303846c9ae9dd0fd2293cb692a284509000ef7
Michael C. Tiberio <MichaelTiberio@users.noreply.github.com>
2020-12-24 10:59:18 -0800

    MVC Movie tutorial, Parts 1-3 (#2)
    * MVC Movie tutorial, Part 2
    
    * WIP: Part 3
    
    * Part 3 complete

---
+++ mct-blog/Controllers/HelloWorldController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace mct_blog.Controllers
+{
+	public class HelloWorldController : Controller
+	{
+		// GET: /HelloWorld/
+		public IActionResult Index() => View();
+
+		// GET: /HelloWorld/Welcome/
+		public IActionResult Welcome(string name, int numTimes = 1)
+		{
+			ViewData["name"] = name;
+			ViewData["numTimes"] = numTimes;
+			return View();
+		}
+	}
+}
