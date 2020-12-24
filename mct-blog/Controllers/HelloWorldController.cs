59e94699590bdc6b972c597c66af8eafe32a24d1
Michael C. Tiberio <MichaelTiberio@users.noreply.github.com>
2020-12-23 16:11:43 -0800

    MVC Movie tutorial, Part 2

---
+++ mct-blog/Controllers/HelloWorldController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace mct_blog.Controllers
+{
+	public class HelloWorldController : Controller
+	{
+		// GET: /HelloWorld/
+		public string Index() => "This is my default action...";
+
+		// GET: /HelloWorld/Welcome/
+		public string Welcome(string name, int ID) => HtmlEncoder.Default.Encode($"Hello {name}, are you {ID} years old?");
+	}
+}
